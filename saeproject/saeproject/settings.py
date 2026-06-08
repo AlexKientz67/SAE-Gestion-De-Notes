dd773888967357ab4b29d4e7028bb3185f21f216
Alexandre KIENTZ <alexkientz6@gmail.com>
2026-06-08 09:46:45 +0200

    patch photo

--- saeproject/saeproject/settings.py
+++ saeproject/saeproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&m%(!2k=4*s)_h9dv%yax^fxf9=q_#hjnuu@$*s5cnhc=qrzn1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
